bde198b779b770f28f256bd1653602f7c31ba885
cgalibern <cgalibern@users.noreply.github.com>
2016-02-14 23:16:28 +0100

    fix test

--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Array', function() {
                   assert.equal([1,2,3].length, 3);
                   assert.equal([1,2,3,9].length, 4);
                   assert.equal([].length, 0);
-                  assert.equal(["bad length test"].length, 0);
+                  assert.equal(["bad length test"].length, 1);
           });
     });});
 
